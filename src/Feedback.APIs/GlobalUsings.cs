39ad8ab882bf4f7fbcfbef19d5847dddf89413c6
Nabi Karampoor <thisisnabi@outlook.com>
2024-10-03 18:46:15 +0330

    Add globalusing file

---
+++ src/Feedback.APIs/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Feedback.APIs;
+global using Feedback.APIs.Endpoints;
+global using Feedback.APIs.Persistence;
+global using Feedback.APIs.Services;
+global using FluentValidation;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Primitives;
+global using System.Reflection;
